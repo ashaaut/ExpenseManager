e383c6efb2c199ca815f96f3c67972e1ebd355f5
ashaaut <ashaautade1998@gmail.com>
2022-05-17 18:46:16 +0530

    Refactor . css property for expense date and price

--- src/components/ExpenseDate.jsx
+++ src/components/ExpenseDate.jsx
@@ -6,8 +6,9 @@ function ExpenseDate(props){
     return(
         <div className="expense-date">
             <div className="expense-date-month">{month}</div>
-            <div className="expense-date-day">{day}</div>
             <div className="expense-date-year">{year}</div>
+            <div className="expense-date-day">{day}</div>
+            
         </div>
     )
 }
