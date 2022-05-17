894d1466e49177c6af1c3a0af4c858f4c95ec2d6
ashaaut <ashaautade1998@gmail.com>
2022-05-17 18:25:26 +0530

    Add .css property for expense

--- src/components/ExpenseDate.jsx
+++ src/components/ExpenseDate.jsx
@@ -1,12 +1,13 @@
+import './../assets/css/ExpenseDate.css'
 function ExpenseDate(props){
     const month=props.date.toLocaleString('en-US',{month:'long'});
     const day=props.date.toLocaleString('en-US',{day:'2-digit'});
     const year=props.date.getFullYear();
     return(
-        <div>
-            <div>{month}</div>
-            <div>{day}</div>
-            <div>{year}</div>
+        <div className="expense-date">
+            <div className="expense-date-month">{month}</div>
+            <div className="expense-date-day">{day}</div>
+            <div className="expense-date-year">{year}</div>
         </div>
     )
 }
