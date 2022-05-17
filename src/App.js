7308792f0c1ab1cca0dd25d165925ab4d28c226b
ashaautade <asha.autade@thoughtworks.com>
2022-05-17 12:36:43 +0530

    Add . expenses and css for expense

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    {
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      title: "New TV",
+      amount: 29400.67,
+      date: new Date(2021, 3, 28),
+    },
+    {
+      title: "Makeup",
+      amount: 294.67,
+      date: new Date(2021, 3, 28),
+    },
+  ];
   return (
     <div className="App">
-        <ExpenseItem/>
+      <ExpenseItem  expenses={expenses}/>
     </div>
   );
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,8 +1,9 @@
 import './ExpenseItem.css'
-function ExpenseItem() {
+function ExpenseItem(props) {
+    const expenses=props.expenses
   return (
     <div className='expense-item'>
-      <div>March 28th 2021</div>
+      <div className='expense-date'>March 28th 2021</div>
       <div className='expense-item-desc'>
         <h2>Bike Insurance</h2>
         <div className='expense-item-price'>$294.80</div>
