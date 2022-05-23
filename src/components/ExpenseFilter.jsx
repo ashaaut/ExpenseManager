2e545b279877010da0a132d701b7be08758004ec
ashaaut <ashaautade1998@gmail.com>
2022-05-23 13:26:26 +0530

    Add . expense filter handld based on year change

--- src/components/ExpenseFilter.jsx
+++ src/components/ExpenseFilter.jsx
@@ -1,17 +1,21 @@
-import './../assets/css/ExpenseFilter.css'
-const ExpenseFilter=()=>{
-    return(
-        <div className="expense-filter">
-            <div className="expense-filter-control">
-                <label className='expense-filter-label'>Filter By Year</label>
-                <select>
-                    <option value="2022">2022</option>
-                    <option value="2021">2021</option>
-                    <option value="2020">2020</option>
-                    <option value="2019">2019</option>
-                </select>
-            </div>
-        </div>
-    )
-}
-export default ExpenseFilter
+import "./../assets/css/ExpenseFilter.css";
+const ExpenseFilter = (props) => {
+    const dropDownChangeHandler=(event)=>{
+        props.filterChangeHandler(event.target.value)
+        // console.log(event.target.value);
+    }
+  return (
+    <div className="expense-filter">
+      <div className="expense-filter-control">
+        <label className="expense-filter-label">Filter By Year</label>
+        <select onChange={dropDownChangeHandler} value={props.selectedYear}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+export default ExpenseFilter;
--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,12 +1,17 @@
 import "./../assets/css/Expenses.css"
 import ExpenseItem from "./ExpenseItem";
 import ExpenseFilter from "./ExpenseFilter";
+import { useState } from "react";
 const Expenses=(props)=> {
   const expenses = props.expenses;
+  const [filteredYear,setFilteredYear]=useState("2022")
+  const filterChangeHandler = (selectedYear)=>{
+    setFilteredYear(selectedYear)
+  }
 
   return (
     <div className="expenses">
-      <ExpenseFilter/>
+      <ExpenseFilter  selectedYear={filteredYear} filterChangeHandler={filterChangeHandler}/>
       {expenses.map((expense) => (
         <ExpenseItem expense={expense} />
       ))}
