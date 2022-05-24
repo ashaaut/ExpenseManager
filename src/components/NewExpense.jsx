998d96d701b1eba38a404ed5494711a5b04983e5
ashaaut <ashaautade1998@gmail.com>
2022-05-24 15:29:34 +0530

    Add. cancel button to cancel form for adding new expense

--- src/components/NewExpense.jsx
+++ src/components/NewExpense.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./../assets/css/NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 const NewExpense = (props) => {
-  const [shouldShowAddNewExpense, setAddNewExpense] = useState(false);
+  const [isFormShow, setFormShow] = useState(false);
 
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
@@ -10,19 +10,23 @@ const NewExpense = (props) => {
       id: Math.random().toString(),
     };
     props.addExpenseDataHandler(expenseData);
-    setAddNewExpense(false)
+    setFormShow(false);
   };
 
   const handleAddNewExpense = () => {
-    setAddNewExpense(true);
+    setFormShow(true);
   };
 
+  const onCancel=()=>{
+    setFormShow(false)
+  }
   return (
     <div className="new-expense">
-      {shouldShowAddNewExpense ? (
-        <ExpenseForm onSaveNewExpenseData={saveExpenseDataHandler} />
-      ) : (
+      {!isFormShow && (
         <button onClick={handleAddNewExpense}>Add New Expense</button>
+      )}
+      {isFormShow && (
+        <ExpenseForm onSaveNewExpenseData={saveExpenseDataHandler}  onCancel={onCancel}/>
       )}
     </div>
   );
